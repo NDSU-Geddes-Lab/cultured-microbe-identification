a26a5532efe9f92e65ccd7e2059d2e48182d7ed4
ndusek <nick.dusek@gmail.com>
2024-05-31 09:27:48 -0500

    Finished and validated process_dada2_seqtab.R

--- process_dada2_seqtab.R
+++ process_dada2_seqtab.R
@@ -1,52 +1,27 @@
 #!/usr/bin/env Rscript
 
-# 1. Create output table from input table with:
-#      - original seq
-#      - f_barcode, r_barcode, and well from matching seq against barcode table
-#      - trimmed_seq
-#      - one column for each plate+well combo, initialized with all zeros
-
-# For each sequence in the seqtab:
-#   1. Trim the primers
-#   2. Identify the forward and reverse barcodes
-#   3. Find the corresponding well number in the barcode table
-#   4. Report the result in the column corresponding to the plate+well combo
+# This script is an R port of Komal Marathe's process_dada2_seqtab.py.
+# It does the following:
+# 
+# 1. Creates an output data frame with the following columns:
+#      - Original sequence from the seqtab (original_seq)
+#      - Forward and reverse matched barcodes (f_barcode, r_barcode)
+#      - Well corresponding to barcode pair (well)
+#      - Trimmed sequence with n bases removed, where n = barcode+primer length (trimmed_seq)
+#      - A column for each plate+well combination, e.g., "plate1_A1", "plate1_A2", ...
+# 2. Then, for each sequence in the seqtab, we do the following:
+#      - Identify the forward and reverse barcodes
+#      - Trim the primers
+#      - Find the corresponding well number in the barcode table
+#      - Report the result in the column corresponding to the plate+well combo
 
 seqtab <- read.csv("output/input_seqtab.csv")
-output_exp <- read.csv("output/processed_data_output.csv")
+# Output from Komal's python code, for comparison
+output_exp <- read.csv("output/processed_data_output.csv", row.names=1)
 barcodes <- read.csv("BC_to_well2.csv")
-
-seq <- seqtab$X[1]
 
 f_primer <- "GTGCCAGCMGCCGCGGTAA"
 r_primer <- "GACTACHVGGGTATCTAATCC"
-
-seq_len <- nchar(seq)
-
-f_len <- nchar(f_primer)
-r_len <- nchar(r_primer)
-
-find_well <- function(seq) {
-  i <- which(startsWith(seq, barcodes$F) & endsWith(seq, barcodes$R))
-  
-  if (length(i) != 1) {
-    return(NA)
-  } else {
-    return(barcodes[i,3])
-  }
-}
-
-wells <- unlist(lapply(seqtab$X, find_well)) # working
-
-df <- data.frame(A=numeric(10), B=numeric(10), C=numeric(10))
-df[,c("D","E","F")] <- 0
-
-cols <- c("G","H","I","J")
-df[,cols] <- 1
-df
-
-# So I can just create a vector of all the plate+well combos 
-# and initialize them to 0 simultaneously
 
 output <- data.frame(original_seq=seqtab$X)
 output[,c("f_barcode","r_barcode","well","trimmed_seq")] <- ""
@@ -54,39 +29,66 @@ output[,c("f_barcode","r_barcode","well","trimmed_seq")] <- ""
 plate_rows <- c("A","B","C","D","E","F","G","H")
 plate_cols <- 1:12
 wells <- as.vector(t(outer(plate_rows, plate_cols, paste, sep="")))
-plate_names <- colnames(seqtab)[-1]
-plate_well_combos <- as.vector(t(outer(plate_names, wells, paste, sep="_")))
+plates <- colnames(seqtab)[-1] # May have to fix index later...
+plate_well_combos <- as.vector(t(outer(plates, wells, paste, sep="_")))
 
 output[,plate_well_combos] <- 0
 
 # Main loop
 for (i in 1:nrow(seqtab)) {
   seq <- seqtab$X[i]
+  
+  f_barcode <- ""
+  r_barcode <- ""
+  well <- "no_well" # To match what Komal has
+  
+  # Find line in barcode map with matching fwd and rev barcodes
   j <- which(startsWith(seq, barcodes$F) & endsWith(seq, barcodes$R))
   
-  if (length(j) == 0) {
-    output[i,"f_barcode"] <- NA
-    output[i,"r_barcode"] <- NA
-    output[i,"well"] <- NA
-    next
+  # There should be either a single match (length(j)==1) 
+  # or no matches (length(j)==0), so if length(j) > 0, set
+  # barcodes and well accordingly.
+  if (length(j) > 0) {
+    f_barcode <- barcodes[j,1]
+    r_barcode <- barcodes[j,2]
+    well <- barcodes[j,3]
   } 
   
-  f_barcode <- barcodes[j,1]
-  r_barcode <- barcodes[j,2]
-  well <- barcodes[j,3]
-  
+  # Populate corresponding output fields
   output[i,"f_barcode"] <- f_barcode
   output[i,"r_barcode"] <- r_barcode
   output[i,"well"] <- well
   
   # Trim sequence
+  # (regardless of whether there is a match or not, since that's what Komal did)
   len_front <- nchar(f_primer) + nchar(f_barcode)
   len_back <- nchar(r_primer) + nchar(r_barcode)
   len_seq <- nchar(seq)
   
+  # Add trimmed sequence to output
   output[i,"trimmed_seq"] <- substr(seq, len_front+1, len_seq-len_back)
+  
+  # If we don't have a barcode match, no point in tabulating plate counts
+  # since we don't know what well they come from.
+  if (length(j) == 0) {
+    next
+  }
   
   # Populate counts
   # For current row, find which plates have non-zero counts
-  # For each plate with a non-zero count, assign total to that plate + this well
+  # For each plate with a non-zero count, assign total to
+  # the appropriate plate+well combo
+  for (plate in plates) {
+    seq_count <- seqtab[i,plate]
+    if (seq_count > 0) {
+      plate_well <- paste(plate, well, sep="_")
+      output[i,plate_well] <- seq_count
+    }
+  }
 }
+
+# Validate by comparing R-generated output with output from Komal's python code
+
+# Row names for output_exp start at 0. Fix before comparing.
+rownames(output_exp) <- 1:nrow(output_exp)
+all.equal(output[,1:1061], output_exp[,1:1061]) #Validation PASSED!
